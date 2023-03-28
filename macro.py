e0c1cc0cc2772e59e453bf2ef2c0ee0d385a95b3
= <donghyun4591@gmail.com>
2023-03-28 16:22:00 +0900

    테스트중

--- macro.py
+++ macro.py
@@ -5,18 +5,23 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import subprocess
-
-# Chrome 오토 봇 감지 우회
-# subprocess.Popen(r'C:\Program Files\Google\Chrome\Application\chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\chromeCookie"')
 
 # 예약 사이트 URL
 url = "https://yeyak.seoul.go.kr/web/reservation/selectReservView.do?rsv_svc_id=S230314055802763586&code=T100&dCode=&sch_order=1&sch_choose_list=&sch_type=&sch_text=%EA%B3%84%EB%82%A8&sch_recpt_begin_dt=&sch_recpt_end_dt=&sch_use_begin_dt=&sch_use_end_dt=&svc_prior=N&sch_reqst_value="
+
+# 테스트 네이버 도메인
+# url = "https://www.naver.com"
+
 
 # 브라우저 옵션 설정
 options = webdriver.ChromeOptions()
 options.add_argument("--start-maximized") # 창 최대화
 options.add_argument("--disable-notifications") # 브라우저 알림 제거
+# options.add_argument("--remote-debugging-port=9222") # 디버깅 모드로 진입
+options.add_argument("--disable-blink-features=AutomationControlled") # 자동화 소프트웨어 차단 기능 해제
+# options.addArguments("disable-infobars");
+# options.AddArguments("enable-automation");
+# options.AddAdditionalCapability("useAutomationExtension", false);
 
 # 크롬 드라이버 실행
 driver = webdriver.Chrome(options=options)
@@ -37,8 +42,8 @@ reservation_button.click()
 # 로그인 정보 입력
 id_input = driver.find_element_by_name("userId")
 pw_input = driver.find_element_by_name("userPwd")
-id_input.send_keys("id 기입") # 여기에 본인의 아이디를 입력하세요.
-pw_input.send_keys("pw 기입") # 여기에 본인의 비밀번호를 입력하세요.
+id_input.send_keys("your_id") # 여기에 본인의 아이디를 입력하세요.
+pw_input.send_keys("your_password") # 여기에 본인의 비밀번호를 입력하세요.
 
 # 로그인 버튼 클릭
 login_button = driver.find_element_by_xpath("//a[@title='로그인']")
@@ -62,5 +67,5 @@ try:
 except:
     print("예약에 실패하였습니다.")
 
-# 브라우저 종료
-driver.quit()
+# 브라우저 닫기
+driver.close()
