c1d4bd5e08beeb138a87a97d6d4c24a711e11998
= <donghyun4591@gmail.com>
2023-03-31 11:33:15 +0900

    Chrome 팅김 현상 해결

--- macro.py
+++ macro.py
@@ -10,11 +10,10 @@ import subprocess
 
 
 # By. 모듈 테스트 로직
-
 # By.selectPeriod = (By.CSS_SELECTOR, '#selectPeriod')
-# By.selectRsvTime = (By.CSS_SELECTOR, '#selectRsvTime')
-# By.userId = (By.CSS_SELECTOR, '#userId')
-# By.userPwd = (By.CSS_SELECTOR, '#userPwd')
+By.selectRsvTime = (By.CSS_SELECTOR, '#selectRsvTime')
+By.userId = (By.CSS_SELECTOR, '#userId')
+By.userPwd = (By.CSS_SELECTOR, '#userPwd')
 
 # 예약 사이트 URL
 url = "https://yeyak.seoul.go.kr/web/reservation/selectReservView.do?rsv_svc_id=S230314055802763586&code=T100&dCode=&sch_order=1&sch_choose_list=&sch_type=&sch_text=%EA%B3%84%EB%82%A8&sch_recpt_begin_dt=&sch_recpt_end_dt=&sch_use_begin_dt=&sch_use_end_dt=&svc_prior=N&sch_reqst_value="
@@ -27,21 +26,22 @@ url = "https://yeyak.seoul.go.kr/web/reservation/selectReservView.do?rsv_svc_id=
 options = webdriver.ChromeOptions()
 options.add_argument("--start-maximized") # 창 최대화
 options.add_argument("--disable-notifications") # 브라우저 알림 제거
-options.add_argument("--remote-debugging-port=9222") # 디버깅 모드로 진입
+# options.add_argument("--remote-debugging-port=9222") # 디버깅 모드로 진입
 options.add_argument("--disable-blink-features=AutomationControlled") # 자동화 소프트웨어 차단 기능 해제
-options.add_argument("disable-infobars")
-options.add_argument("enable-automation")
+# options.add_argument("disable-infobars")
+# options.add_argument("enable-automation")
 # options.AddAdditionalCapability("useAutomationExtension", false);
-options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.5563.147 Safari/537.36")
-options.add_argument("app-version=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.5563.147 Safari/537.36")
+# options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.5563.147 Safari/537.36")
+# options.add_argument("app-version=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.5563.147 Safari/537.36")
 
 # 크롬 드라이버 실행
 driver = webdriver.Chrome(options=options)
 driver.get(url)
 
 # 예약 가능한 날짜 선택
-date_select = Select(driver.find_element(By.selectPeriod)) # 예약 날짜 선택 셀렉트 박스
+# date_select = Select(driver.find_element(By.selectPeriod)) # 예약 날짜 선택 셀렉트 박스
 # date_select = Select(driver.find_element(By.XPATH, '//select[@id="selectPeriod"]')) # 예약 날짜 선택 셀렉트 박스
+date_select = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'selectPeriod')))
 date_select.select_by_index(1) # 두번째(내일) 날짜 선택
 
 # 예약 가능한 시간 선택
